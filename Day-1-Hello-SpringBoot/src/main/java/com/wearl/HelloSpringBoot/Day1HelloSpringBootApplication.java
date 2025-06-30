625fe4590a7bb73ac7e8ac32d20a96a79f2e32b4
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-06-30 23:40:20 +0530

    Day 1: Spring Boot Hello World setup with REST endpoint

---
+++ Day-1-Hello-SpringBoot/src/main/java/com/wearl/HelloSpringBoot/Day1HelloSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.wearl.HelloSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day1HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day1HelloSpringBootApplication.class, args);
+	}
+
+}
