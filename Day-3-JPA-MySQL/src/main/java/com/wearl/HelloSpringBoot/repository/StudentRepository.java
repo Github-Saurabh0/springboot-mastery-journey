65d0cc90067653503a04cd4858c75bc23061a249
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-03 22:16:03 +0530

    Create StudentRepository extending JpaRepository

---
+++ Day-3-JPA-MySQL/src/main/java/com/wearl/HelloSpringBoot/repository/StudentRepository.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wearl.HelloSpringBoot.repository;
+
+/**
+ *
+ * @author Saurabh
+ */
+import com.wearl.HelloSpringBoot.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
