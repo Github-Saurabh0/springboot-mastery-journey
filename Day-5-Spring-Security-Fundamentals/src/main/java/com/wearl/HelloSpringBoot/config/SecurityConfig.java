8c219619f26c0b6e8ef17f9f61dcc2eaba13d442
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-08 09:18:52 +0530

    Add basic Spring Security config - in-memory auth with username & password

---
+++ Day-5-Spring-Security-Fundamentals/src/main/java/com/wearl/HelloSpringBoot/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wearl.HelloSpringBoot.config;
+
+/**
+ *
+ * @author Saurabh
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeHttpRequests()
+            .anyRequest().permitAll();
+        return http.build();
+    }
+}
+
+
