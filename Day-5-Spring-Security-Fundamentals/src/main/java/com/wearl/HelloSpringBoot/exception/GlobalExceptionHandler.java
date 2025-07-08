b132e0b4650ef4a44129e3af8de74bb9a1b76c15
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-08 09:22:16 +0530

    Refactor: Included exception handler to support secured API errors

---
+++ Day-5-Spring-Security-Fundamentals/src/main/java/com/wearl/HelloSpringBoot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.wearl.HelloSpringBoot.exception;
+
+/**
+ *
+ * @author Saurabh
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        StringBuilder errors = new StringBuilder();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+            errors.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("\n")
+        );
+        return new ResponseEntity<>(errors.toString(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(StudentNotFoundException.class)
+    public ResponseEntity<String> handleStudentNotFound(StudentNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
