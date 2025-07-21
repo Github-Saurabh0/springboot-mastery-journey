e4a23a8e1ee48c9410d96b7e55dc5b78bcc55bff
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-21 23:29:09 +0530

    create StudentDTO with id, name, email, and course fields

---
+++ Day-9-Testing/src/main/java/com/wearl/HelloSpringBoot/dto/StudentDTO.java
@@ -0,0 +1,53 @@
+package com.wearl.HelloSpringBoot.dto;
+
+public class StudentDTO {
+    private int id;
+    private String name;
+    private String email;
+    private String course;
+
+    // Default Constructor
+    public StudentDTO() {
+    }
+
+    // All-Args Constructor
+    public StudentDTO(int id, String name, String email, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
