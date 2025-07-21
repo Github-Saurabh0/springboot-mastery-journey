627e29f287c55b7eecf85cfccca57d0ff8f899e2
Saurabh kushwaha <162017120+Github-Saurabh0@users.noreply.github.com>
2025-07-21 23:31:49 +0530

    add unit test for StudentService

---
+++ Day-9-Testing/src/test/java/com/wearl/HelloSpringBoot/service/StudentServiceTest.java
@@ -0,0 +1,50 @@
+package com.wearl.HelloSpringBoot.service;
+
+import com.wearl.HelloSpringBoot.dto.StudentDTO;
+import com.wearl.HelloSpringBoot.exception.ResourceNotFoundException;
+import com.wearl.HelloSpringBoot.mapper.StudentMapper;
+import com.wearl.HelloSpringBoot.model.Student;
+import com.wearl.HelloSpringBoot.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentServiceTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private StudentMapper studentMapper;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student) {
+        studentRepository.save(student);
+    }
+
+    public void updateStudent(int id, Student updatedStudent) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with ID: " + id));
+
+        student.setName(updatedStudent.getName());
+//        student.setEmail(updatedStudent.getEmail());
+
+        studentRepository.save(student);
+    }
+
+    public void deleteStudent(int id) {
+        Student student = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found with ID: " + id));
+        studentRepository.delete(student);
+    }
+     public StudentDTO getStudentById(int id) {
+    Student student = studentRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Student not found with ID: " + id));
+    return studentMapper.toDTO(student); // Assuming you have a mapper
+}
+}
